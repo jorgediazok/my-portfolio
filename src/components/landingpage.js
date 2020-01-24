bf415d725005e5eae4e6e3d1fb6de64ba4e76223
Jorge <jorgediazok@gmail.com>
2020-01-24 17:36:57 -0300

    algunos cambios

---
+++ src/components/landingpage.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class LandingPage extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Landing Page</h1>
+      </div>
+    );
+  }
+}
+
+export default LandingPage;
